ff08b60b7c38e93ab116611b4ac69c4ec6a23a20
Gökcan <87297423+gokcaneren@users.noreply.github.com>
2022-02-22 15:55:36 +0300

    Add files via upload

---
+++ KodluyoruzAlistirma1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodluyoruzAdSoyad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adınız:");
+            string ad = Console.ReadLine();
+            Console.Write("Soyadınız:");
+            string soyad = Console.ReadLine();
+            Console.WriteLine("Merhaba {0} {1}", ad, soyad);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
