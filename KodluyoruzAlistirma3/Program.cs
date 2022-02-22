b83ea4c1959b58e53c61c317e1595350540adaed
Gökcan <87297423+gokcaneren@users.noreply.github.com>
2022-02-22 15:56:46 +0300

    Add files via upload

---
+++ KodluyoruzAlistirma3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodluyoruzAlistirma3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kısa Kenar:");
+            double kisaKenar = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Uzun Kenar:");
+            double uzunKenar = Convert.ToDouble(Console.ReadLine());
+            double alan = kisaKenar * uzunKenar;
+            double cevre = 2 * (kisaKenar + uzunKenar);
+            Console.WriteLine("Alan:{0}", alan);
+            Console.WriteLine("Çevre:{0}", cevre);
+
+
+            Console.ReadKey();
+        }
+    }
+}
