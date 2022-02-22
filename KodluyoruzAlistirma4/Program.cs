6b67fa54ad61e9c5933f0f21fefbc95447f8bb75
Gökcan <87297423+gokcaneren@users.noreply.github.com>
2022-02-22 15:57:10 +0300

    Add files via upload

---
+++ KodluyoruzAlistirma4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodluyoruzAlistirma4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lütfen bir sayı giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            if (sayi%2==0)
+            {
+                Console.WriteLine("Çift Sayı");
+            }
+            else 
+            {
+                Console.WriteLine("Tek Sayı");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
