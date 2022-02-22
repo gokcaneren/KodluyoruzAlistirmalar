d44a064f2a389104122e52ebe656b4b33e3cf2b5
Gökcan <87297423+gokcaneren@users.noreply.github.com>
2022-02-22 15:56:10 +0300

    Add files via upload

---
+++ KodluyoruzAlistirma2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KodluyoruzAlistirma2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("1.Sayı:");
+            int s1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("2.Sayı:");
+            int s2= Convert.ToInt32(Console.ReadLine());
+            int result = s1 + s2;
+            Console.WriteLine("{0} ve {1} Sayısının Toplamı : {2}", s1, s2, result);
+
+
+            Console.ReadKey();
+        }
+    }
+}
